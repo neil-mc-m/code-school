f76811f5870851097c14630e1811219f6169b44e
neil-mc-m <neilo2000@gmail.com>
2022-09-12 23:39:54 +0100

    rearrange notes

--- week_4/class_8/app.js
+++ week_4/class_8/app.js
@@ -23,45 +23,4 @@ let newArray = myArray.map(function (value) { // ['GREEN', 'RED', 'BLACK']
 
 console.log(newArray.join(' '));
 
-// functions
-
-//named function
-function output (parameter) {
-    return parameter.toUpperCase();
-}
-
-// usage
-console.log(output('dublin coding school full stack developer'));
-
-// anonymous functions
-let anonFunction = function (parameter) {
-    return parameter.toUpperCase();
-}
-
-// usage
-console.log(anonFunction('full stack developer courses'));
-
-
-// objects
-let myObject = {};
-myObject.type = 'ford';
-myObject.fuel = 'diesel';
-myObject.mileage = 120000;
-myObject.value = 10000;
-
-// object with anonymous functions
-let car = {
-    milesPerLiter: 0,
-    drive: function (distance, litersOfFuelUsed) {
-        this.milesPerLiter = distance / litersOfFuelUsed;
-    },
-    calculateMilesPerLiter: function () {
-        return this.milesPerLiter;
-    }
-}
-
-const ford = Object.create(car);
-
-ford.drive(20, 2);
-
-myContent.innerHTML = '<div>Ford miles per liter:' + ford.calculateMilesPerLiter() + '</div>';
+myContent.innerHTML = newArray.join(' ');
