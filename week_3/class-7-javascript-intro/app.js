ace39f6060c2b43af5a233dfe4ed5ef427802409
neil-mc-m <neilo2000@gmail.com>
2022-09-12 19:41:16 +0100

    classwork

--- week_3/class-7-javascript-intro/app.js
+++ week_3/class-7-javascript-intro/app.js
@@ -2,7 +2,41 @@ let myContent = document.getElementById('app');
 
 
 
-let message = 'hello everyone';
 
 
-myContent.innerHTML = message;
+// numbers
+let myNumber = Number();
+let stringNumber = '1098';
+
+let myStringNumber = myNumber.toString();
+
+
+let myNum = 123.456789;
+
+let myNumText = myNum.toString();
+console.log(myNumText);
+
+let myNumExp = myNum.toExponential();
+console.log(myNumExp);
+
+let myNumPrecision = myNum.toPrecision(10);
+console.log(myNumPrecision);
+
+
+
+
+// strings
+let message = 'This is the TEST string';
+
+// if (message.indexOf('L') !== -1) {
+//     myContent.innerHTML = message;
+// }
+// let myArray = message.split(' ')
+
+// array
+
+
+let myArray = ['red', 'blue', 'green', 'yellow', 'black']
+
+myContent.innerHTML = myArray[0];
+
---
+++ week_4/class_8/readme.md
@@ -0,0 +1,7 @@
+## Exercise: 
+- Create two variables for the first name and last name, assign values to them, then create a third variable that combines the first and last name variables, and print the value of the third variable to the console.
+
+- Use string “Dublin Coding School” and replace coding from the string with CODING.
+- New String should look like “Dublin CODING School”
+
+- Reverse string “Dublin Coding School” to “School Coding Dublin”
