1ea85c479045d374f3a3e087df84fa6d7bdbb247
neil-mc-m <neilo2000@gmail.com>
2022-09-13 20:07:00 +0100

    classwork

--- week_4/class_8/app.js
+++ week_4/class_8/app.js
@@ -2,14 +2,27 @@ let myContent = document.getElementById('app');
 
 // number
 let myNumber = Number('20');
-console.log(myNumber);
+
+let myOtherNumber = 10;
+
+console.log(myOtherNumber.toString());
 
 // string
 let myString = 'Dublin Coding School     ';
 console.log(myString.trim()); // 'Dublin Coding School'
 console.log(myString.indexOf('u')); // 1
 console.log(myString.toUpperCase()); // 'DUBLIN CODING SCHOOL'
+console.log(myString.length); // 'DUBLIN CODING SCHOOL'
 
+
+let day = 'thursday';
+let time = '4 o clock';
+
+const details = 'I will be available ' + day + ', at around ' + time + ', if that suits?';
+// String interpolation
+const detailsInter = `I will be available ${day}, at around ${time}, if that suits?`;
+console.log(detailsInter);
+console.log(details);
 
 // array
 let myArray = [];
@@ -17,10 +30,39 @@ myArray.push('green'); // ['green']
 myArray.push('red'); // ['green', 'red']
 myArray.push('black') // ['green', 'red', 'black']
 myArray.indexOf('red');// 1
+myArray.unshift('blue'); // adds to the start
+myArray.shift(); // remove from start of array
+myArray.pop(); // remove from end of array
+let mySecondArray = ['yellow', 'brown', 'purple'];
+let myJoinedArray = myArray.concat(mySecondArray);
+// console.log(myJoinedArray);
+
+
 let newArray = myArray.map(function (value) { // ['GREEN', 'RED', 'BLACK']
-    return value.toUpperCase()
+    return value.toUpperCase();
 })
 
-console.log(newArray.join(' '));
+console.log(myFunction(myArray, mySecondArray));
+console.log(myFunction(myArray, myJoinedArray));
 
-myContent.innerHTML = newArray.join(' ');
+function myFunction (array1, array2) {
+    return array1.concat(array2)
+}
+// console.log(newArray.join(' '));
+
+
+let fiveDaysInMinutes = calculateNoOfMinutes(5);
+
+// myContent.innerHTML = fiveDaysInMinutes;
+
+
+function calculateNoOfMinutes(days) {
+    const numberOfHours = days * 24;
+    const numberOfMinutes = numberOfHours * 60;
+
+    return numberOfMinutes;
+}
+
+let today = new Date();
+
+myContent.innerHTML = today.toDateString();
---
+++ week_4/class_9/readme.md
@@ -0,0 +1,5 @@
+- Create a function called “reverseWordOrder” that takes one string parameters, “value”
+The function should reverse the order of the words in the string and return the updated string
+- Create a function called “logEvent” that takes two parameters, a string “eventName” and a date object “occurred”
+It should print a string to the console in the following format: “Event: eventName occurred on occurred”
+E.g. “Event: user log in occurred on Sat, 12 Feb 2022 12:04:35 GMT”
