01e454ad1919a1f4eec4b5795779b936a9e75895
neil-mc-m <neilo2000@gmail.com>
2022-09-19 20:13:48 +0100

    week 5

---
+++ week_5/class_11/app.js
@@ -0,0 +1,33 @@
+let myContent = document.getElementById('app');
+
+const cars = ['BMW', 'Audi', 'VolksWagen', 'Skoda', 'Volvo'];
+//
+let text = '';
+
+// for (let i = 0; i < cars.length; i++) {
+//     htmlLinks = htmlLinks + `<a href="https://www.google.com/search?q=${cars[i]}" target="_blank">${cars[i]}</a><br/>`;
+// }
+
+// for (let car of cars) {
+//     text += `<a href="https://www.google.com/search?q=${car}" target="_blank">${car}</a><br/>`;
+// }
+
+// let count = 0;
+
+// while (count < 10) {
+//     text += "The number is " + count;
+//     count ++;
+// }
+//
+// let i = 10;
+//
+// do {
+//     text += i + '<br>';
+//     i++;
+// } while (i < 5);
+//
+
+
+
+myContent.innerHTML = text;
+
---
+++ week_5/class_11/readme.md
@@ -0,0 +1,10 @@
+## Class 11 - javascript loops
+
+## Write a function that finds the number of even numbers and remove the odd ones from array of numbers.
+
+
+- Inside the function write a for loop
+- Inside the for loop find if a number is even or odd.
+- Hint - number%2
+- If mod is 0 - push number to a new array
+- Return new array
