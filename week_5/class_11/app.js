6edc608bc7884c31c6a62c096a0107565c3d04a1
neil-mc-m <neilo2000@gmail.com>
2022-09-20 09:15:53 +0100

    fetch example, tidy up loops examples

--- week_5/class_11/app.js
+++ week_5/class_11/app.js
@@ -1,16 +1,25 @@
 let myContent = document.getElementById('app');
-
-const cars = ['BMW', 'Audi', 'VolksWagen', 'Skoda', 'Volvo'];
-//
+const cars = ['volvo', 'bmw', 'skoda', 'volkswagon'];
 let text = '';
 
+
+/**
+ * for loops
+ **/
+
 // for (let i = 0; i < cars.length; i++) {
-//     htmlLinks = htmlLinks + `<a href="https://www.google.com/search?q=${cars[i]}" target="_blank">${cars[i]}</a><br/>`;
+//     text += `<a href="https://www.google.com/search?q=${cars[i]}" target="_blank">${cars[i]}</a><br/>`;
 // }
 
-// for (let car of cars) {
-//     text += `<a href="https://www.google.com/search?q=${car}" target="_blank">${car}</a><br/>`;
-// }
+for (let car of cars) {
+    text += `<a href="https://www.google.com/search?q=${car}" target="_blank">${car}</a><br/>`;
+}
+
+myContent.innerHTML = text;
+
+/**
+ * while and do/while loops
+ **/
 
 // let count = 0;
 
@@ -28,6 +37,18 @@ let text = '';
 //
 
 
-
-myContent.innerHTML = text;
+// optional classwork solution
+// const winningLottoNumbers = [10, 23, 11, 73, 44, 57];
+// const userNumbers = [10, 23, 11, 44, 57, 57];
+//
+//
+// function matchingNumbers (userNumbers, winningLottoNumbers) {
+//     let matchingNumbers = 0;
+//     for (let i = 0;i < userNumbers.length; i++) {
+//         if (winningLottoNumbers.includes(userNumbers[i])) {
+//             matchingNumbers = matchingNumbers + 1;
+//         }
+//     }
+//     return matchingNumbers;
+// }
 
---
+++ week_5/fetch-example/app.js
@@ -0,0 +1,15 @@
+let myContent = document.getElementById('app');
+
+async function getBreweryData () {
+    let response = await fetch('https://api.openbrewerydb.org/breweries?by_city=dublin&per_page=25', {mode: 'cors'})
+    return await response.json();
+}
+
+getBreweryData().then(function (breweries)  {
+    let htmlLinks = ''
+    for (let brewery of breweries) {
+        htmlLinks = htmlLinks + `<a href="${brewery.website_url}" target="_blank">${brewery.name}</a><br/>`;
+    }
+
+    myContent.innerHTML = htmlLinks;
+});
