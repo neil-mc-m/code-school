f4a16db700317dde533a05320acda5efd7d1a82d
neil-mc-m <neilo2000@gmail.com>
2022-09-26 20:46:52 +0100

    php files

---
+++ week_6/header.php
@@ -0,0 +1,9 @@
+<?php
+$links = ['index.php' => 'home', 'contact-us.php' => 'Contact us'];
+?>
+<header>
+    <h1>MAIN TITLE</h1>
+    <nav>
+        <a href="./index.php">home</a>
+    </nav>
+</header>
---
+++ week_6/index.php
@@ -0,0 +1,17 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="../css/main.css">
+</head>
+<body>
+<?php include 'header.php'?>
+<main>
+
+</main>
+</body>
+</html>
