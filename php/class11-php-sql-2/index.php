7396ea053e0d4f4dfe54c5c8ca1f6dea092dd4b4
neil-mc-m <neilo2000@gmail.com>
2023-02-17 10:24:07 +0000

    class 11 search form and date diff

--- php/class11-php-sql-2/index.php
+++ php/class11-php-sql-2/index.php
@@ -5,21 +5,44 @@ $databaseUser = 'php-user';
 $databaseHost = 'localhost';
 $database = 'recipes';
 
-if (isset($_POST['q'])) {
-    var_dump($_POST['q']);
+
+if (isset($_GET['q'])) {
+    // how to calculate the difference in days between 2 dates
+    // the variable $diff should contain the number of days between the 2 dates
+    $startDate = new DateTime('2023-02-16');
+    $endDate = new DateTime('2023-02-23');
+    $diff = $startDate->diff($endDate)->d;
+
+    //step 1 - create the connection to the DB
     $connection = new mysqli($databaseHost, $databaseUser, $databasePassword, $database);
-    var_dump($connection);
+
+    // step 2 - prepare the statement
     $statement = $connection->prepare('SELECT * from recipe_list WHERE `name` = ?');
+
+    // step 3 - bind parameters to the statement and execute statement
     $statement->bind_param('s', $_GET['q']);
     $statement->execute();
 
-    // step 2
+    // step 4 - retrieve the result
     $result = $statement->get_result();
 
-    // step 3
+    // step 5 - check for number of results and fetch into a php format (an array usually)
     if ($result->num_rows > 0) {
 
         $recipe = $result->fetch_assoc();
+
+        $recipeId = $recipe['id'];
+
+        $recipeName = $recipe['name'];
+
+        $recipeDescription = $recipe['description'];
+    }
+
+    if ($result->num_rows === 0) {
+
+        $noResults = "<p>No results found</p>";
+
+    }
 }
 
 ?>
@@ -31,17 +54,47 @@ if (isset($_POST['q'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Title</title>
 </head>
+
 <body>
 <section class="container">
 
 
     <form action="" method="GET">
-        <label for="username">Username</label>
-        <input id="username" name="q" placeholder="Search recipes" type="text"/>
+
+        <label for="search">Search</label>
+        <input id="search" name="q" placeholder="Search recipes" type="text"/>
+
+        <select name="room_type">
+            <option value="double">double</option>
+            <option value="single">single</option>
+        </select>
 
         <button type="submit">Search</button>
+
     </form>
 
+
+</section>
+<section class="container">
+    <div>
+        <h1>Search results</h1>
+        <?php
+        if (isset($recipeName) && isset($recipeDescription) && isset($recipeId)) {
+
+            echo "<p>$recipeName</p>";
+
+            echo "<p>$recipeDescription</p>";
+
+            echo "<a href='ingredients.php?id=$recipeId'>See ingredients</a>";
+        }
+
+        if (isset($noResults)) {
+
+            echo $noResults;
+
+        }
+        ?>
+    </div>
 </section>
 </body>
 </html>
